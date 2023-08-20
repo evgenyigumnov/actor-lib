15726e700b443172b34e1b7c06d172295e6f1a0c
Evgeny Igumnov <igumnovnsk@gmail.com>
2023-08-20 13:17:28 +0600

    publish

---
+++ examples/ping_pong/src/echo.rs
@@ -0,0 +1,43 @@
+use std::sync::Arc;
+use actorlib::Actor;
+
+pub struct Echo;
+
+#[derive(Debug)]
+pub enum Message {
+    Ping,
+}
+
+#[derive(Debug)]
+pub enum Response {
+    Pong {counter: u32},
+}
+
+#[derive(Debug,Clone)]
+pub struct State {
+    pub counter: u32,
+}
+
+impl Echo {
+    pub async fn new() -> Arc<Actor<Message, State, Response>> {
+
+        let state = State {
+            counter: 0,
+        };
+
+        Actor::new("echo".to_string(), move |ctx| {
+            Box::pin(async move {
+                match ctx.mgs {
+                    Message::Ping => {
+                        println!("Got pong");
+                        let mut state_lock = ctx.state.lock().await;
+                        state_lock.counter += 1;
+                        Ok(Response::Pong{counter: state_lock.counter})
+                    }
+                }
+
+          })
+        }, state, 100000).await
+    }
+
+}
--- examples/ping_pong/src/main.rs
+++ examples/ping_pong/src/main.rs
@@ -1,9 +1,33 @@
+use std::thread;
+use tokio::runtime::Runtime;
 use actorlib::*;
+use crate::echo::*;
+
+mod echo;
 
 #[tokio::main]
 async fn main() -> Result<(), BoxDynError> {
+    let echo = Echo::new().await;
 
-    println!("Ctrl-C to stop");
-    tokio::signal::ctrl_c().await?;
+    println!("Sent Ping");
+    echo.send(Message::Ping).await;
+
+    println!("Sent Ping and ask response");
+    let pong = echo.ask(Message::Ping).await.unwrap();
+    println!("Got Pong: {:?}", pong);
+
+    println!("Sent Ping and wait response in callback");
+    echo.callback(Message::Ping, move |result| {
+        Box::pin(async move {
+            let respone = result?;
+            if let Response::Pong { counter } = respone {
+                println!("Got Pong with counter: {}", counter);
+            }
+            Ok(())
+        })
+    }).await;
+
+    echo.stop();
+    thread::sleep(std::time::Duration::from_secs(1));
     Ok(())
 }
